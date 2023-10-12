ee909ae6b26eb6c198f546d0ec8b7393ffd403ca
dga48 <darrickgunawan@gmail.com>
2023-10-11 22:43:09 -0700

    test

---
+++ darrick's messing around file/sockets_play.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+// server side
+
+int server() {
+
+}
